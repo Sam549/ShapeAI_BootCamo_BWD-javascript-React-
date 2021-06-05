ffde06c4faa40a41819a4a49b5f2965d447ef0b6
Sam549 <65754695+Sam549@users.noreply.github.com>
2021-06-05 12:14:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <div class="para">
+        <h2>JavaScript and React.js</h2>
+        <p>
+          This was an amazing bootcamp taken by shaurya Sinah coveres everything
+          from scratch including Javascript React.js, HTML.
+        </p>
+      </div>
+      <div class="para">
+        <h2>JavaScript and React.js</h2>
+        <p>
+          This was an amazing bootcamp taken by shaurya Sinah coveres everything
+          from scratch including Javascript React.js, HTML.
+        </p>
+      </div>
+      <div class="para">
+        <h2>JavaScript and React.js</h2>
+        <p>
+          This was an amazing bootcamp taken by shaurya Sinah coveres everything
+          from scratch including Javascript React.js, HTML.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
